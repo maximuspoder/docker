0432a3f3c35f864e38cb705326fd510f88e20100
= <developer.fernando@gmail.com>
2020-04-28 13:35:26 -0300

    .

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(dev.magento.digitalphoto.dev www.dev.magento.digitalphoto.dev)
+domains=(dev.magento.digitalphoto.dev)
 rsa_key_size=4096
 data_path="./docker/certbot"
 email="developer.fernando@gmail.com" # Adding a valid address is strongly recommended
@@ -67,7 +67,7 @@ esac
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker-compose run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
+  certbot certonly --webroot -w /src/www/certbot \
     $staging_arg \
     $email_arg \
     $domain_args \
